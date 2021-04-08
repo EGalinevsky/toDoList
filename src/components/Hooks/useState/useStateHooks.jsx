d0bd977761f34a1e7d2f48927db5e2d0fb712d26
Eugene <65033002+EGalinevsky@users.noreply.github.com>
2021-04-08 21:24:56 +0300

    useEffect

--- src/components/Hooks/useState/useStateHooks.jsx
+++ src/components/Hooks/useState/useStateHooks.jsx
@@ -1,13 +1,34 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-export const Hooks1 = () =>{
+export const Hooks1 = () => {
+  const [count, setCount] = useState(0);
+  const [type, setType] = useState("users");
+  const [data, setData] = useState([])
 
-    const [count, setCount] = useState(0)
-    return (
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, [type]);
+
+  return (
+    <div>
+      <p>колчество кликов {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Увеличить</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>уменьшить</button>
+      <br />
+      <br />
+      <div>
+        <p>{type}</p>
+        
+        <button onClick={() => setType("users")}>users</button>
+        <button onClick={() => setType("posts")}>posts</button>
+        <button onClick={() => setType("todos")}>todos</button>
+
         <div>
-            <p>колчество кликов {count}</p>
-            <button onClick={() =>setCount(prev => prev + 1) }>Увеличить</button>
-            <button onClick={() =>setCount(prev => prev - 1)}>уменьшить</button>
+            {JSON.stringify(data, null, 2)}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
